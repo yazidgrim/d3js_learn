177f9b75bbd458199cd2cc31ed239f6407640066
Yazid Grim <mohamed.yazid.grim@avanade.com>
2021-12-10 16:40:33 -0500

    Refractor: putting attributes in JSON instead of calling functions everytime

--- Charts/render.js
+++ Charts/render.js
@@ -13,17 +13,12 @@ svg.selectAll("rect")
     .data(dataset)
     .enter()
     .append("rect")
-        .attr("x", function(d, i) {
-            return i * (w / dataset.length);
-        })
-        .attr("y", function(d) {
-            return h - (d * 4);
-        })
-        .attr("width", w / dataset.length - padding)
-        .attr("height", function(d) {
-            return d * 4;
-        })
-        .attr("fill", function(d) {
-            return "rgb(" + (d * 10) + ", 0, 0)"
+        .attr({
+            x: function(d, i) {return i * (w / dataset.length);},
+            y: function(d) {return h - (d * 4);},
+            width: w / dataset.length - padding,
+            height: function(d) {return d * 4;},
+            fill: function(d) {return "rgb(" + (d * 10) + ", 0, 0)"}
         });
+
 
